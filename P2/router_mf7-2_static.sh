23f0d640534e8460dc9c2edb13ad38531a9c5b8d
matteo fernandes <mf7compte@gmail.com>
2026-05-11 22:10:11 +0200

    static multicast seems works

--- P2/router_mf7-2_static.sh
+++ P2/router_mf7-2_static.sh
@@ -10,6 +10,7 @@ ip addr flush dev eth1
 ip link set eth0 up
 ip link set eth1 up
 ip addr add 10.0.0.2/24 dev eth0
+ip addr add 30.1.1.4/24 dev eth1
 
 ip link add br0 type bridge
 ip link set br0 up
@@ -18,3 +19,4 @@ ip link set eth1 master br0
 ip link add vxlan10 type vxlan id 10 local 10.0.0.2 remote 10.0.0.1 dstport 4789 dev eth0
 ip link set vxlan10 up
 ip link set vxlan10 master br0
+bridge fdb append 00:00:00:00:00:00 dev vxlan10 dst 10.0.0.1
